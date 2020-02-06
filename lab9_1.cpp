24c414cb6fa5dbef81de6c4420fcd7fb81456820
Dexshine <b_o_y_s@hotmail.com>
2020-02-06 19:18:28 +0700

    break at 0, add student count,fix  total count

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -4,9 +4,10 @@ using namespace std;
 int main(){
 	int count[5] = {}; //Declare array count for counting A,B,C,D,F and initialize all element = 0
 	char grade;
-	cout << "Please input grade of each student (A-F) or input 0 to exit.";
+	cout << "Please input grade of each student (A-F) or input 0 to exit."<<endl;
+	int i=1;
 	do{
-		cout << "Student [" << "]:";
+		cout << "Student ["<< i << "]:";
 		cin >> grade; //The loop must be terminated when grade = '0'
 		if(grade=='A') // if grade is A
 			count[0]++;
@@ -18,17 +19,21 @@ int main(){
 			count[3]++;
 		else if(grade=='F')
 			count[4]++;
-		else // grade is wrong input
+		else if(grade=='0')
+			break;
+		else{ // grade is wrong input
 			cout << "Wrong input. Please input again."<<endl;
-		 
-	}while(grade!=0);
+			--i;
+		}
+		++i;
+	}while(true);
 	
-	cout << "In total ? students."<<endl;
+	cout << "In total " << i-1 << " students."<<endl;
 	cout << "A = " << count[0] <<",";
-	cout << "B = " << count[1] <<",";
-	cout << "C = " << count[2] <<",";
-	cout << "D = " << count[3] <<",";
-	cout << "F = " << count[4] ;	
+	cout << " B = " << count[1] <<",";
+	cout << " C = " << count[2] <<",";
+	cout << " D = " << count[3] <<",";
+	cout << " F = " << count[4] ;	
 	
 	return 0;
 }
