e357c068bcfc61043422b44713990cdc3ad4c8e7
Dexshine <b_o_y_s@hotmail.com>
2020-02-06 21:32:39 +0700

    ver1

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,3 +1,26 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include<iostream>
+#include<fstream>
+#include<cmath>
+#include<cstdlib>
+#include<string>
+
+using namespace std;
+int main()
+{
+    ifstream score("score.txt");
+    double count=0,sum=0,zsquarex=0;
+    string text;
+    while(getline(score,text)){
+        sum += atof(text.c_str());
+        zsquarex += pow(atof(text.c_str()),2);
+        ++count;
+
+    }
+    double deviation,mean;
+    mean = sum/count;
+    deviation = sqrt(((1/count) * (zsquarex)) - pow(mean,2));
+    cout << "Number of data = " << count << " ";
+    cout << "Mean = " << mean << " ";
+    cout << "Standard deviation = " << deviation;
+    return 0;
+}
