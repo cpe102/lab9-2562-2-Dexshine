dc7bbc3c64b36aae4b4d7315f76e51e91b9e8ad3
Dexshine <b_o_y_s@hotmail.com>
2020-02-06 19:43:55 +0700

    ver1

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -2,3 +2,26 @@
 //cout << textline;
 //cout << "-------------------- SOTUS ---------------------";
 //Don't forget to change cout to output filestream
+#include<iostream>
+#include<string>
+#include<cstdlib>
+#include<fstream>
+using namespace std;
+
+int main()
+{
+    ifstream cheerbook;
+    cheerbook.open("F:\\cpp\\261102\\lab9-2562-2-Dexshine\\cheerbook.txt");
+    ofstream cheerbook_copy;
+    cheerbook_copy.open ("F:\\cpp\\261102\\lab9-2562-2-Dexshine\\cheerbook_copy.txt");
+    cheerbook_copy << "“-------------------- SOTUS ---------------------”"<<endl;
+    string text;
+    while(getline(cheerbook,text)){
+        cheerbook_copy << text << endl;
+    }
+    cheerbook_copy << "“-------------------- SOTUS ---------------------”"<<endl;
+    cheerbook.close();
+    cheerbook_copy.close();
+
+    return 0;
+}
